ec6befaef6f7fac7490ea96eba76300113ba3896
alek-3 <jaianism.of.grotius@hotmail.co.jp>
2018-06-16 13:31:11 +0900

    平均スコアの推移をExcelシートに出力

--- stats.py
+++ stats.py
@@ -1,12 +1,15 @@
 import csv
 import os
+import openpyxl
 
 
 class Stats:
     def show_score_stats(self, file_name):
         csv_path = os.path.join("scoresheet", file_name)
         self.get_play_count(csv_path)
-        self.calc_avarage(csv_path)
+        averages = self.calc_avarage(csv_path)
+        self.write_excel(averages)
+
 
     def calc_avarage(self, csv_path):
         """レベルごとの平均点を計算し、出力する"""
@@ -29,6 +32,9 @@ class Stats:
                 level_score_dict[level]["平均スコア"] = score["スコア合計"] / score["譜面数"]
 
             print(sorted(level_score_dict.items()))
+            print(sorted(level_score_dict.items())[16][1]['平均スコア'])
+            return sorted(level_score_dict.items())
+
 
 
     def get_play_count(self, csv_path):
@@ -40,11 +46,42 @@ class Stats:
                 play_count += int(row["プレー回数"])
             print("譜面プレー回数：{play_count}".format(play_count=play_count))
 
+    def write_excel(self, averages):
+        """日付と平均スコアの推移をExcelシートに書きこむ"""
+        wb = openpyxl.load_workbook('sdvx_score_averages.xlsx')
+        sheet = wb.active
+
+        # 二行目以降
+        level = 1
+        for average in averages:
+            sheet.cell(row=file_number + 1, column=level + 1).value = average[1]['平均スコア']
+            level += 1
+
+        wb.save('sdvx_score_averages.xlsx')
+
+    def write_header(self):
+        wb = openpyxl.load_workbook('sdvx_score_averages.xlsx')
+        sheet = wb.active
+        sheet.cell(row=1, column=1).value = '日付＼レベル'
+        for col in range(2,22):
+            sheet.cell(row=1, column=col).value = col - 1
+
+        wb.save('sdvx_score_averages.xlsx')
+
+
+
+
 
 stats = Stats()
 files = os.listdir("scoresheet")
 files_file = [f for f in files if os.path.isfile(os.path.join("scoresheet", f))]
 
+stats.write_header()
+
+# ファイル番号を行番号として使う
+file_number = 1
+
 for file in files_file:
     stats.show_score_stats(file)
+    file_number += 1
 
